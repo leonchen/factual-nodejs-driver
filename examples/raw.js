42934718391f49f1b21163613c0e6fa5a75e0b91
leonchen <leon@factual.com>
2012-05-14 21:57:03 +0800

    add comments for raw

--- examples/raw.js
+++ examples/raw.js
@@ -3,4 +3,7 @@ var factual = helper.factual;
 var qs    = require('querystring');
 
 var filters = qs.stringify({filters:'{"locality":{"$in":["los angeles","newyork"]}}'});
+// request data with the raw api url
+// the response object will be
+// the default factual api response(s)
 factual.raw('/t/places?q=starbucks&sort=locality:asc&limit=10&offset=10&'+filters, helper.output);
